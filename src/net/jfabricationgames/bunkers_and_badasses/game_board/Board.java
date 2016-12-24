ff9bef3360787197c03c8617d0608586425e273c
Tobias <tobias-fassbender@gmx.de>
2016-12-24 10:31:25 +0100

    new storing of images: images are wraped in ImageIcon class and serialized directly

--- src/net/jfabricationgames/bunkers_and_badasses/game_board/Board.java
+++ src/net/jfabricationgames/bunkers_and_badasses/game_board/Board.java
@@ -1,5 +1,6 @@
 package net.jfabricationgames.bunkers_and_badasses.game_board;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -7,7 +8,7 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 public class Board implements Serializable {
 	
@@ -17,6 +18,8 @@ public class Board implements Serializable {
 	private List<Region> regions;
 	private transient BufferedImage baseImage;
 	private String name;
+	
+	private ImageIcon imageWrapper;
 
 	public BufferedImage displayField() {
 		//TODO
@@ -33,9 +36,10 @@ public class Board implements Serializable {
 	 * 		The ObjectOutputStream that is used to write down the file.
 	 */
 	private void writeObject(ObjectOutputStream out) throws IOException {
+		imageWrapper = new ImageIcon(baseImage);
 		out.defaultWriteObject();//serialize the object but the transient fields (the baseImage)
-		out.writeInt(1);//write the number of images down (needed also if there is only one)
-		ImageIO.write(baseImage, "png", out);//write the image at the end of the file
+		//out.writeInt(1);//write the number of images down (needed also if there is only one)
+		//ImageIO.write(baseImage, "png", out);//write the image at the end of the file
 	}
 	/**
 	 * Read the object from a serialized file. The not-serializabel parts (the buffered image) is stored separately at the end of the file.
@@ -48,8 +52,27 @@ public class Board implements Serializable {
 	 */
 	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
 		in.defaultReadObject();//load all the fields that could be serialized (not the transient fields like baseImage)
-		in.readInt();//read the number of stored images (its only one so no need to store the value)
-		baseImage = ImageIO.read(in);//read the image file
+		baseImage = loadBaseImage(imageWrapper);
+		imageWrapper = null;
+		//in.readInt();//read the number of stored images (its only one so no need to store the value)
+		//baseImage = ImageIO.read(in);//read the image file
+	}
+	
+	/**
+	 * Create a BufferedImage from an ImageIcon by drawing it.
+	 *  
+	 * @param imageIcon
+	 * 		The ImageIcon.
+	 * 
+	 * @return
+	 * 		The BufferedImage.
+	 */
+	private BufferedImage loadBaseImage(ImageIcon imageIcon) {
+		BufferedImage img = new BufferedImage(imageIcon.getIconWidth(), imageIcon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+		Graphics g = img.createGraphics();
+		imageIcon.paintIcon(null, g, 0, 0);
+		g.dispose();
+		return img;
 	}
 	
 	public List<Field> getFields() {
